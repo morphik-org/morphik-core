d1150c7894c7a03c4bf64f9db41d5e6defd5976e
Adityavardhan Agrawal <aa729@cornell.edu>
2025-04-11 20:50:28 -0700

    Science graph evals

---
+++ evaluations/Science graphs (SciER)/data_loader.py
@@ -0,0 +1,24 @@
+import json
+import os
+from data_structures import Dataset
+
+
+def load_jsonl(jsonl_path: str) -> list:
+    data = []
+    with open(jsonl_path) as f:
+        for line in f:
+            data.append(json.loads(line))
+    return data
+
+
+# ========= usage example for load_jsonl function with LLM input =======
+# dataset = load_jsonl('./SciER/LLM/test.jsonl')
+# sent = dataset[0]
+# print(sent.keys())
+# print(sent['sentence'])
+# print('----------------')
+# print(sent['ner'])
+# print('----------------')
+# print(sent['rel'])
+# print('----------------')
+# print(sent['rel_plus'])
