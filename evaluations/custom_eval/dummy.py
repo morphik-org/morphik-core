40c81c4863cec4b2bf5bb9b09a554cec14ebda6b
Kevin Su <kevinsu2024@u.northwestern.edu>
2025-08-19 12:32:52 -0400

    polish

--- evaluations/custom_eval/dummy.py
+++
@@ -1,6 +0,0 @@
-import torch
-   
-if torch.backends.mps.is_available():
-    print("MPS is available! PyTorch can use the Apple Silicon GPU.")
-else:
-    print("MPS is not available. Make sure you're on an Apple Silicon Mac and have the correct PyTorch version installed.")
