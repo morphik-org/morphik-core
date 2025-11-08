5e099e6e8d9f252c6e21d1a6fa4601b866b1b24d
Adityavardhan Agrawal <aa729@cornell.edu>
2025-11-07 17:04:00 -0800

    Add Promise.withResolvers polyfill for compatibility and update PDFViewer to use it

--- ee/ui-component/components/pdf/PDFViewer.tsx
+++ ee/ui-component/components/pdf/PDFViewer.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import "@/lib/polyfills/promise-with-resolvers";
+
 import React, { useState, useCallback, useRef, useEffect, useMemo } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
 import { Button } from "@/components/ui/button";
